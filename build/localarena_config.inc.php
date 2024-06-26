18b3f7a7c5c0ac9475366fbdead4d5ab1ff43b73
Kevin Kelley <kelleyk@kelleyk.net>
2024-06-26 13:09:51 -0700

    WIP: Clean up validation code

--- build/localarena_config.inc.php
+++ build/localarena_config.inc.php
@@ -9,80 +9,53 @@ echo '*** XXX: Loading localarena_config.inc.php for effortlesswc...' . "\n";
 // JSON Schema for all notifs, args, etc. that the server sends to the client.
 require_once 'modules/php/client_interface_schema.inc.php';
 
-use \Opis\JsonSchema\{Validator, ValidationResult, Helper};
+use Opis\JsonSchema\{Validator, ValidationResult, Helper};
 
-use Opis\JsonSchema\Errors\{
-ErrorFormatter,
-  ValidationError,
-  };
+use Opis\JsonSchema\Errors\{ErrorFormatter, ValidationError};
 
 global $validator;
 $validator = new Validator();
 
-// global $schemas;
-// $schemas = [];
-global $combined_schema;
 $combined_schema = json_decode(CLIENT_INTERFACE_SCHEMA, /*associative=*/ false);
-// foreach ($combined_schema['definitions'] as $schema_name => $schema) {
-//   echo '*** ZZZ: about to parse...' . "\n";
-//   $schemas[$schema_name] = $validator->loader()->loadObjectSchema((object) $schema);
-//   echo '*** ZZZ: just parsed...' . "\n";
-// }
+$validator->resolver()->registerRaw((object) $combined_schema, 'http://localarena.example.com/schema.json');
 
-// $validator->loader()->loadObjectSchema((object) $combined_schema);
-
-$validator->resolver()->registerRaw((object)$combined_schema, 'http://localarena.example.com/schema.json');
-
-function validateJsonSchema($schema_name, $data)
+function validateJsonSchema($schema_name, $data, $msg)
 {
   global $validator;
-  // global $schemas;
-  global $combined_schema;
 
-  // if (!array_key_exists($schema_name, $schemas)) {
-  //   throw new \BgaVisibleSystemException(
-  //     'Client-interface schema validation failed: schema not found: ' . $schema_name
-  //   );
-  // }
-  // $schema = $schemas[$schema_name];
-  // $result = $validator->validate($data, $schema);
-
-  // XXX: Unlike some of the other validation methods, `uriValidation()` returns a single ValidationError or null.
-  $err = $validator->uriValidation((object)$data, 'http://localarena.example.com/schema.json' . '#/definitions/' . $schema_name);
+  // N.B.: Unlike some of the other validation methods, `uriValidation()` returns a single ValidationError or null.
+  $err = $validator->uriValidation(
+    (object) $data,
+    'http://localarena.example.com/schema.json' . '#/definitions/' . $schema_name
+  );
   if ($err !== null) {
-
-    // throw new \Exception(get_class($err));
-
     $formatter = new ErrorFormatter();
     throw new \Exception(
-      'Schema validation error: ' .
-        implode("\n\n--------------\n\n", $formatter->format($err, false)));
-
-    // throw new \Exception('Schema validation error on keyword "'.$result->keyword().'": ' . $result->message() . "\n\n" . 'Schema ' . print_r($result->schema(), true) . ' and data '
-    //                      // . "\n" . print_r($result->data(), true)
+      'Failed to validate ' .
+        $msg .
+        ' against schema `' .
+        $schema_name .
+        '`: ' .
+        implode("\n\n--------------\n\n", $formatter->format($err, false))
+    );
   }
-
-  // // XXX: Eventually, we should improve this to show all errors (`getErrors()`) and not just the first one.
-  // $err = $result->getFirstError();
-  // if ($err !== null) {
-  //   throw $err;
-  // }
 }
 
-function localarenaConfigureGame($localarenaGameConfig) {
+function localarenaConfigureGame($localarenaGameConfig)
+{
   // Called to validate the return value of `getAllDatas()`.
   $localarenaGameConfig->registerAllDatasValidator(function ($all_datas) {
-    validateJsonSchema('Gamedatas', $all_datas);
+    validateJsonSchema('Gamedatas', $all_datas, 'gamedatas');
   });
 
   // Called to validate the return value of each state args function.
   $localarenaGameConfig->registerArgsValidator(function ($state_name, $data) {
-    validateJsonSchema(ucfirst($state_name) . 'Args', $data);
+    validateJsonSchema(ucfirst($state_name) . 'Args', $data, 'args for state "' . $state_name . '"');
   });
 
   // Called to validate the contents of each notif.
   $localarenaGameConfig->registerNotifValidator(function ($notif_name, $data) {
-    validateJsonSchema(ucfirst($notif_name) . 'Notif', $data);
+    validateJsonSchema(ucfirst($state_name) . 'Notif', $data, 'data for notif "' . $notif_name . '"');
   });
 
   echo '*** XXX: done with `localarenaConfigureGame()...' . "\n";
