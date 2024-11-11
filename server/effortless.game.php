06a3b8a43a140369fe090fb708dea9002c101685
Kevin Kelley <kelleyk@kelleyk.net>
2024-11-10 19:54:34 -0800

    Remove stale comment

--- server/effortless.game.php
+++ server/effortless.game.php
@@ -193,47 +193,12 @@ class Effortless extends TableBase
     }
   }
 
+  // -----------
+  // Misc
+  // -----------
+
   function argGameEnd()
   {
-    // // XXX: Return all scoring info so that the client can draw the end-game screen.
-
-    // foreach (Seat::getAll($this->world()) as $seat) {
-    //   $scoring_details = new PlayerScoringData();
-
-    //   $items = [];
-
-    //   foreach ($seat->cards($world) as $card) {
-    //     if ($card instanceof AttributeCard) {
-    //       // XXX:
-    //     } elseif ($card instanceof ArmorCard) {
-    //       // XXX:
-    //     } elseif ($card instanceof ItemCard) {
-    //       // XXX:
-    //     } else {
-    //       throw new \BgaVisibleSystemException('Unexpected card type during scoring.');
-    //     }
-    //   }
-    // }
-
-    // // Armor (1/4/8/13 per set)
-
-    // // Items (points as printed on the item, if we have the attributes to utilize it)
-
-    // // Attributes
-    // //
-
-    // // Settings (top to bottom)
-
-    // // Tie breakers:
-    // //
-    // // - Highest single attribute score
-    // // - Highest single scored item
-    // // - Most complete armor sets
-
-    // return array_merge($this->renderBoardState(), [
-    //   'scoring' => \Effortless\calculateScores($this->world()),
-    // ]);
-
     return array_merge(parent::argGameEnd(), [
       'scoringDetail' => \Effortless\calculateScores($this->world()),
     ]);
